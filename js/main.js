4c82f80d150ddc5a22498317ceed66dff83aff21
yonnjy <133613780+yonnjy@users.noreply.github.com>
2023-05-15 16:08:12 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    // document.querySelector('h1').style.display = 'none';
+    // $('h1').css({ display: 'none' })
+    // $('h1').hide();
+    //  var => const, let
+    const option = {
+        anchors: ['01', '02', '03'],
+        afterRender: function () {
+            setTimeout(function () { $('.section').eq(0).addClass('on') });
+        },
+        onLeave: function (index, nextIndex, direction) {
+            console.log(index, nextIndex, direction);
+            $('h1 span').text(nextIndex);
+            $('nav li').eq(nextIndex - 1).addClass('on')
+                .siblings().removeClass('on');
+            $('.section').eq(nextIndex - 1).addClass('on')
+                .siblings().removeClass('on');
+        },
+    }
+    $('.main_full').fullpage(option);
+    // $('.section').eq(0).addClass('on');
+})
